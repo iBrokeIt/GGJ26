45537123be0bdb0759e6264055470f9e93d2b4cc
Idan Tal <idantal@Idans-MacBook-Air.local>
2026-01-29 21:52:02 +0200

    update scene

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -4,8 +4,11 @@ using UnityEngine.InputSystem;
 public class PlayerContoller : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
 
     public InputActionReference moveAction;
+    public InputActionReference jumpAction;
+    private bool isGrounded = false;
 
     Rigidbody2D rb;
 
@@ -14,11 +17,29 @@ public class PlayerContoller : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+
+    void Update()
     {
         // If no InputAction assigned, fall back to keyboard/gamepad probing
         Vector2 moveInput = moveAction.action.ReadValue<Vector2>();
         rb.linearVelocity = new Vector2(moveInput.x, 0) * moveSpeed;
+        float jumpInput = jumpAction.action.ReadValue<float>();
+        if (jumpInput > 0.1f && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
 }
