8c6972e0dea4a4c02aa6e0b3d9d15584bc97a2c8
Inbal Nitzani <inbalnitzani@gmail.com>
2026-01-30 01:10:29 +0200

    disapearing

--- Assets/Scripts/Disappear.cs
+++ Assets/Scripts/Disappear.cs
@@ -5,11 +5,17 @@ public class Disappear : MonoBehaviour
 {
     public float timeToDisappear = 2.0f;
     public float timeToAppear = 5.0f;
-    public bool startDisappeared = false;
+
+
+    private bool startDisappeared = false;
+
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -23,19 +29,20 @@ public class Disappear : MonoBehaviour
         yield return new WaitForSeconds(timeToDisappear);
 
         // Make the object disappear
-        gameObject.SetActive(false);
+        spriteRenderer.enabled = false;
+        collider.enabled = false;
 
         // Wait for the specified time
         yield return new WaitForSeconds(timeToAppear);
 
         // Make the object appear again
-        gameObject.SetActive(true);
- 
+        spriteRenderer.enabled = true;
+        collider.enabled = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !startDisappeared)
         {
             startDisappeared = true;
             StartCoroutine(DisappearAndAppear());
