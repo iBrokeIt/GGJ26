5fe26d939ec9b7b41b11653192341e5ffe54a215
listguy <nitzanli@wix.com>
2026-01-30 10:08:47 +0200

    add inventory

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance;
+
+    public event Action<string, int> OnInventoryChanged;
+
+    private Dictionary<string, int> inventory = new Dictionary<string, int>();
+
+    void Awake()
+    {
+        // Singleton Setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    public void AddItem(string itemName, int amount = 1)
+    {
+        var newAmount = 0;
+        if (inventory.ContainsKey(itemName))
+        {
+            inventory[itemName] += amount;
+            newAmount = inventory[itemName];
+        }
+        else
+        {
+            inventory.Add(itemName, amount);
+            newAmount = amount;
+        }
+        OnInventoryChanged.Invoke(itemName, newAmount);
+        Debug.Log($"Collected: {itemName}. Total: {inventory[itemName]}");
+    }
+
+    public int GetItemCount(string itemName)
+    {
+        if (inventory.ContainsKey(itemName))
+        {
+            return inventory[itemName];
+        }
+        return 0;
+    }
+}
