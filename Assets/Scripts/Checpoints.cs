368efb240e4783fefab586536f214992cfd6d718
Idan Tal <idant14@gmail.com>
2026-01-30 18:14:41 +0200

    add torch

--- Assets/Scripts/Checpoints.cs
+++ Assets/Scripts/Checpoints.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class Checkpoints : MonoBehaviour
 {
+    public GameObject flame;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             GameManager.Instance.UpdateCheckpoint(transform.position);
-
+            flame.SetActive(true);
         }
     }
 }
