82ec3ec2f2517b4a0ca86c1f89dd265e3da8b6f7
listguy <nitzanli@wix.com>
2026-02-24 20:50:24 +0200

    Tutorial

--- Assets/Scripts/TutorialEndZone.cs
+++ Assets/Scripts/TutorialEndZone.cs
@@ -3,11 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class TutorialEndZone : MonoBehaviour
 {
+    [SerializeField] float transitionDelay =1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Main");
+            Invoke("LoadScene", transitionDelay);
         }
     }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    
 }
